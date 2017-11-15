102255027ce3c934d9d10c92cb5cd36f2a8bd6a1
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-11-14 22:17:53 -0500

    initial commit of actual code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+// var Promise = require('bluebird') // I only need this for Promise.map and Promise.all
+var request = require('request')
+
+function send_post(url, value) {
+  return new Promise((resolve, reject) => {
+    request.post(url, value, (err, response, body) => {
+        err && reject(err, response)
+        body && resolve(body)
+    })
+  })
+}
+
+class Album extends Component {
+  render() {
+    var data = this.props.data
+    console.log("rendering",this.props.title)
+    return (
+      <p>{data.collectionName} - {data.releaseDate.getFullYear()}</p>
+    )
+  }
+}
+
+class Artist extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+    send_post("https://itunes.apple.com/lookup?id="+this.props.artistId+"&entity=album")
+      .then(results => JSON.parse(results).results.slice(1))
+      .then(albums => this.setState({albums: albums}))
+      .catch(console.log)
+  }
+  render() {
+    console.log("rendering",this.props.artistName)
+    return (
+      <div className={this.props.artistName}>
+        <h1 className="title">{this.props.artistName}</h1>
+        <p className="contents">{this.props.contents + " "+this.state.albums.length}</p>
+        { 
+          if (!this.state.albums){
+            return (
+              <p>Getting albums...</p>
+            )
+          }
+          this.state.albums.map(album => {
+            return (
+              <Album key={album.collectionId} data={album} />
+            )
+          })
+        }
+      </div>
+   )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Artist artistId="20006408" artistName="Regina Spektor" contents="contents"/>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
