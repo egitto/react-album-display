caa9fb1aa54fb13e21b6d231d0b6d27da1ba631b
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-11-15 23:36:50 -0500

    hover-over to hide label, label includes price and link to place, adjusted sizes

--- src/App.js
+++ src/App.js
@@ -12,28 +12,47 @@ class Border extends Component {
   }
 }
 
+function enlargeArtworkUrl(url,size){
+  return url.replace(/\/100x100/,"/"+size+"x"+size)
+}
+
 function albumConstructor(album){
   return (
-    <Album 
-      key={album.collectionId} 
-      coverArt={album.artworkUrl100} 
-      title={album.collectionName} 
-      year={album.releaseDate.slice(0,4)}
-      artist={album.artistName} 
-      trackCount={album.trackCount}/>
+    <a>
+      <Album 
+        key={album.collectionId} 
+        coverArt={enlargeArtworkUrl(album.artworkUrl100,350)} 
+        title={album.collectionName} 
+        year={album.releaseDate.slice(0,4)}
+        artist={album.artistName} 
+        trackCount={album.trackCount}
+        collectionPrice={album.collectionPrice}
+        collectionViewUrl={album.collectionViewUrl}
+        albumId={album.collectionId}/>
+    </a>
   )
 }
 
 class Album extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {tracks:[],status:"loading tracks"}
+    axios.get("https://itunes.apple.com/lookup?collectionId="+this.props.albumId+"")
+      .then(results => {console.log(results); return results.data.results.slice(1)})
+      .then(albums => this.setState({tracks: albums}))
+      .then(this.setState({status:"tracks loaded"}))
+      .catch(e => {this.setState({status:""+e}); console.log(e)})
+  }
   render() {
     var data = this.props
     console.log("rendering",data.title)
     return (
       <Border>
-        <p className="AlbumLabel">
-          <p className="AlbumTitle">{data.title}</p>
+        <a className="AlbumLabel">
+          <p className="AlbumTitle">{data.title}<br/>
+          <a href={data.collectionViewUrl}>${data.collectionPrice}</a></p>
           <p className="AlbumArtist">Tracks: {data.trackCount}<br/>{data.artist} - {data.year}</p>
-        </p>
+        </a>
         <img src={data.coverArt} className="CoverArt" alt={"Cover of "+data.title} />    
       </Border>
     )
@@ -89,6 +108,8 @@ class App extends Component {
     return (
       <div className="App">
         <Artist artistId="20006408" artistName="Regina Spektor" contents="A list of albums released by Regina Spektor"/>
+        {//<Artist artistId="551695" artistName="David Bowie" contents="A list of albums released by David Bowie"/>
+        }
       </div>
     )
   }
